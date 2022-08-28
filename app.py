c05d03eee96c9d8b3335bf822a18f99824f3eb91
Gabriel-Martins <GMartinsss@hotmail.com>
2022-08-28 14:34:32 -0300

    Teste deployment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratório DevOps - FIAP 8ASO"
+    return "Laboratório DevOps - FIAP 8ASO - Gabriel"
 
 if __name__ == '__main__':
     app.run()
